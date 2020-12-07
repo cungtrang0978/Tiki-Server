7a87930ceeda1b615757e63888f79204fc603b33
Thang Nguyen <cungtrang0978@gmail.com>
2020-12-07 11:44:17 +0700

    update: insertShipping.php

--- insertShipping.php
+++ insertShipping.php
@@ -5,7 +5,7 @@ if(isset($_POST['idTransact'])){
 
 	$idTransact = $_POST['idTransact'];
 
-	$idShipper;
+	$idShipper = 0;
 
 	// get userId not in any transact = new shipper
 	$query1 = "SELECT user.id FROM user
@@ -18,17 +18,25 @@ if(isset($_POST['idTransact'])){
 
 	if ($data_1 = mysqli_query($connect, $query1)) {
 		$row_1 = mysqli_fetch_assoc($data_1);
+		
 		if(isset($row_1)){
-			$idShipper = $row_1;
+			$idShipper = $row_1['id'];
+
+			//insert
+			$query_insert_shipping = "INSERT INTO `shipping`(`idTransact`, `idShipper`) VALUES ('$idTransact', '$idShipper')";
+			if(mysqli_query($connect, $query_insert_shipping)){
+				echo "insert_success";
+			} else echo "insert_fail";
 		}else{
 			//count shipper
-			$query2 = "SELECT COUNT(*) FROM user WHERE user.roleId = 2";
+			$query2 = "SELECT COUNT(*) AS count FROM user WHERE user.roleId = 2";
 			if($data_2 = mysqli_query($connect, $query2)){
 				$row_2 = mysqli_fetch_assoc($data_2);
 				if(isset($row_2)){
-					$count = $row_2;
+					$count = $row_2['count'];
+		
 					//get userId of shiiper has oldest transact.
-					$query3 ="SELECT shipping.idShipper FROM (SELECT shipping.idShipper FROM `shipping`
+					$query3 ="SELECT shipping.idShipper FROM (SELECT shipping.* FROM `shipping`
       				                  ORDER BY shipping.id DESC
       				                  LIMIT $count) as shipping
 							ORDER BY `shipping`.`id` ASC                        
@@ -36,20 +44,19 @@ if(isset($_POST['idTransact'])){
 					if($data_3 = mysqli_query($connect, $query3)){
 						$row_3 = mysqli_fetch_assoc($data_3);
 						if(isset($row_3)){
-							$idShipper = $row_3;
+							$idShipper = $row_3['idShipper'];
+							//insert
+							$query_insert_shipping = "INSERT INTO `shipping`(`idTransact`, `idShipper`) VALUES ('$idTransact', '$idShipper')";
+							if(mysqli_query($connect, $query_insert_shipping)){
+								echo "insert_success";
+							} else echo "insert_fail";
 						}
 					}
 				}
 			}
 		}		
 	}
+} else echo "non_value";
 
-	$query_insert_shipping = "INSERT INTO `shipping`(`idTransact`, `idShipper`) VALUES ('$idTransact', '$idShipper')";
-	
-	if(mysqli_query($connect, $query_insert_shipping)){
-		echo "insert_success";
-	} else echo "insert_fail";
-}else{
-	echo "non_value";
-}
+
 ?>
